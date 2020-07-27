54464c9881e7bff99d9c017f6b4f167fdd7065ad
Chester <chesterdeveloper2000@gmail.com>
2020-07-27 12:14:11 +0800

    when you're in the modal then you want to dismiss it, you can now click anywhere outside it

--- script.js
+++ script.js
@@ -14,5 +14,10 @@ showModal = () => {
 }
 
 
+
 // modal event listener
 modalShow.addEventListener("click", showModal)
+modalClose.addEventListener("click", () => modal.classList.remove("show-modal"))
+window.addEventListener("click" , e => {
+    (e.target === modal) ? modal.classList.remove("show-modal") : false
+})
